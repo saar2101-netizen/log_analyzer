943c620cd47c80e19aab16bfa70a10b73db11628
Saar Efrat <saar2101@gmail.com>
2026-02-10 12:09:30 +0200

    Add find_suspicions function

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,6 @@
+import checks
+import config
+import csv
 
 def check_size(logs):
     large_size = [log[1] for log in logs if int(log[-1]) > 5000]
@@ -27,3 +30,25 @@ def port_protocol(logs):
         protocol = log[4]
         port_and_protocol[port] = protocol
     return port_and_protocol
+
+
+def find_suspicions(logs):
+    EXTERNAL = checks.find_external_ip(logs)
+    SENSITIVE = config.find_sensitive_port(logs)
+    LARGE = check_size(logs)
+    NIGHT = checks.night_activity(logs)
+    s_dict = dict()
+    for log in logs:
+        lst_suspicions = []
+        s_ip = log[1]
+        if s_ip in EXTERNAL:
+            lst_suspicions.append("EXTERNAL_IP")
+        if s_ip in SENSITIVE:
+            lst_suspicions.append("SENSITIVE_PORT")
+        if s_ip in LARGE:
+            lst_suspicions.append("LARGE_PACKET")
+        if s_ip in NIGHT:
+            lst_suspicions.append("NIGHT_ACTIVITY")
+        if lst_suspicions:
+            s_dict[s_ip] = lst_suspicions
+    return s_dict
