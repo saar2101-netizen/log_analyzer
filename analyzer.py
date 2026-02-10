80139c2fd6d05ee12cc93a0d26c099917bbfdc36
Saar Efrat <saar2101@gmail.com>
2026-02-10 11:13:20 +0200

    Add count_requests function

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,18 @@ def check_size(logs):
     large_size = [log[1] for log in logs if int(log[-1]) > 5000]
     return large_size
 
+
 def logs_tags(logs):
     tags_log = [log + ["LARGE"] if int(log[-1]) > 5000 else log + ["NORMAL"] for log in logs]
     return tags_log
+
+
+def count_requests(logs):
+    requests = dict()
+    for log in logs:
+        s_ip = log[1]
+        if s_ip in requests:
+            requests[s_ip] += 1
+        else:
+            requests[s_ip] = 1
+    return requests
