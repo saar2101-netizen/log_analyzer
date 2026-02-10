ed6ccff0279fabd8c0dccccd376fdd917c24d991
Saar Efrat <saar2101@gmail.com>
2026-02-10 10:23:37 +0200

    Fixing the output of the log

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 def find_sensitive_port(logs):
-    sensitive_port = [log for log in logs if log[3] in ["22", "23", "3389"]]
+    sensitive_port = [log[1] for log in logs if log[3] in ["22", "23", "3389"]]
     return sensitive_port
