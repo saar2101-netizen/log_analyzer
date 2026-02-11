59dc208024cf8b72e4eb6476c000fcdadf5b9cf3
Saar Efrat <saar2101@gmail.com>
2026-02-11 14:44:24 +0200

    Fixing some bug in find_suspicions function

--- analyzer.py
+++ analyzer.py
@@ -32,26 +32,53 @@ def port_protocol(logs):
     return port_and_protocol
 
 
+import datetime
+
+
 def find_suspicions(logs):
-    EXTERNAL = checks.find_external_ip(logs)
-    SENSITIVE = config.find_sensitive_port(logs)
-    LARGE = check_size(logs)
-    NIGHT = checks.night_activity(logs)
     s_dict = dict()
+
+    start_time = datetime.time(0, 0, 0)
+    end_time = datetime.time(6, 0, 0)
+    sensitive_ports = ["22", "23", "3389"]
+
     for log in logs:
-        lst_suspicions = []
+        timestamp_str = log[0]
         s_ip = log[1]
-        if s_ip in EXTERNAL:
+        dst_port = log[3]
+        size = int(log[5])
+
+        lst_suspicions = []
+
+        if not s_ip.startswith("10.") and not s_ip.startswith("192.168."):
             lst_suspicions.append("EXTERNAL_IP")
-        if s_ip in SENSITIVE:
+
+        if dst_port in sensitive_ports:
             lst_suspicions.append("SENSITIVE_PORT")
-        if s_ip in LARGE:
+
+        if size > 5000:
             lst_suspicions.append("LARGE_PACKET")
-        if s_ip in NIGHT:
-            lst_suspicions.append("NIGHT_ACTIVITY")
+
+        try:
+            log_time = datetime.datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S").time()
+            if start_time <= log_time <= end_time:
+                lst_suspicions.append("NIGHT_ACTIVITY")
+        except ValueError:
+            pass
+
         if lst_suspicions:
-            s_dict[s_ip] = lst_suspicions
-    return s_dict
+            if s_ip not in s_dict:
+                s_dict[s_ip] = set()
+
+            s_dict[s_ip].update(lst_suspicions)
+
+    final_dict = {}
+    for ip, suspicions_set in s_dict.items():
+        final_dict[ip] = list(suspicions_set)
+
+    return final_dict
+
+
 
 def filtering_suspicions(logs):
     sus_dict = find_suspicions(logs)
